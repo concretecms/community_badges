1174b986818108a88800476e2d029dbd0fe1a414
wordish <42463480+wordish@users.noreply.github.com>
2022-04-20 18:10:38 -0700

    Update award_given.php

--- mail/award_given.php
+++ mail/award_given.php
@@ -18,5 +18,5 @@ $subject = t('Awarded Award – %s', $userBadge->getBadge()->getName());
 if ($userBadge->isGivenBySystem()) {
     $body = t("Congratulations! You awarded the award %s from the website.", $userBadge->getBadge()->getName());
 } else {
-    $body = t("Congratulations! You awarded the award %s from the user %s.", $userBadge->getBadge()->getName(), $userBadge->getAwardGrant()->getUser()->getUserName());
+    $body = t("Congratulations! You awarded the award %1$s from the user %2$s.", $userBadge->getBadge()->getName(), $userBadge->getAwardGrant()->getUser()->getUserName());
 }
